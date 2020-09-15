b201cb7e230e419f2b15654cf8808d4a1b280ffc
julianoaklein <julianoaklein@gmail.com>
2020-09-15 07:21:21 -0300

    Update infrastructure
    Adjust mysql container

--- chat-infrastructure/db/mysql/init/init.sql
+++ chat-infrastructure/db/mysql/init/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `db`.`chat_message` (
     `id` BIGINT NOT NULL,
-    `user_id` VARCHAR(100) NOT NULL,
+    `user_id` VARCHAR(36) NOT NULL,
     `chat_room_id` VARCHAR(20) NOT NULL,
     `message` VARCHAR(500) NOT NULL,
     `created_on` DATETIME NOT NULL,
@@ -8,9 +8,8 @@ CREATE TABLE `db`.`chat_message` (
 );
 
 CREATE TABLE `db`.`user` (
-    `id` BIGINT NOT NULL,
-    `userName` VARCHAR(30) NOT NULL,
+    `id` VARCHAR(36) NOT NULL,
+    `username` VARCHAR(30) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
-    `message` VARCHAR(500) NOT NULL,
     PRIMARY KEY (`id`)
 );
